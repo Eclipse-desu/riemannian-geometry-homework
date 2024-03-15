1277d3872bb47eed15a5c186b678743a9953d5c5
Eclipse-desu <hilberto.eclipse@gmail.com>
2024-03-15 17:16:08 +0800

    Finished homework 2.

--- make.bat
+++ make.bat
@@ -2,8 +2,11 @@ if "%1" == "" (
     echo "Please input file number."
 ) else (
     cd homeworks
-    xelatex %1 -output-directory=../pdf
-    cd ../pdf
+    xelatex %1
+    xelatex %1 
+    move /y ./%1.pdf ../pdf/%1.pdf
     del *.aux *.log *.out
+    cd ../pdf
+    code %1.pdf
     cd ..
 )
